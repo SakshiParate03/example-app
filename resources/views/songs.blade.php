660a3c3f827b0daaea49ad4c4fa66a22b8cc80c9
SakshiParate03 <sakshiparate03@gmail.com>
2023-11-25 15:26:29 +0530

    Update songs view to read from database

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,15 +1,2 @@
-
-<html>
-<table>
-<tr>
-<th>Title</th>
-<th>Artist</th>
-</tr>
-@foreach ($songs as $song)
-<tr>
-<td>{{ $song->Title}}</td>
-<td>{{ $song->Artist}}</td>
-</tr>
-@endforeach
-</table>
-</html>
+<x-layouts.main>
+</x-layouts.main>
