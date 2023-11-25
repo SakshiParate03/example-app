0eb9987905d9bf6f26518ca96e2f004a964824e9
SakshiParate03 <sakshiparate03@gmail.com>
2023-11-25 15:00:00 +0530

    Create Song database model

---
+++ resources/views/Songs.blade.php
@@ -0,0 +1,33 @@
+<div>
+Title: {{ $song -> getTitle() }}
+</div>
+
+<html>
+    <body>
+        <center>
+    <h1>Sakshi's Vegetable store</h1>
+    <table border='3'>
+        <tr>
+            <th>Vegetable name</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>Aaloo</td>
+            <td>30 Rs</td>
+        </tr>
+        <tr>
+            <td>Gobhi</td>
+            <td>50 Rs</td>
+        </tr>
+        <tr>
+            <td>Bhindi</td>
+            <td>20 Rs</td>
+        </tr>
+        <tr>
+            <td>Baigan</td>
+            <td>60 Rs</td>
+        </tr>
+    </table>
+</center>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
+use Practicals\Song;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,4 +21,28 @@ Route::get('/veggies', function () {
 Route::get('/veggies/{veggieName}', function (string $veggieName) {
     return $veggieName;
 })->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo','gobhi','Baigan', 'Bhindi', 'Aaloo','Gobhi']);
+
+Route::get('/songs', function () {
+    return "Songs";
+});
+Route::get('/songs', function () {
+    return view('songs');
+});
+Route::get('/songs', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+  
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+  
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+  
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  });
+
+
 ?>
